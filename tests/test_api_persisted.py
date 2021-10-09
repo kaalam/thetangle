5995a35da665e48dcec98747108a4263ef0c343b
kaalam <kaalam@kaalam.ai>
2021-10-09 13:01:08 +0200

    Implements some elementary cases

--- tests/test_api_persisted.py
+++ tests/test_api_persisted.py
@@ -1,13 +1,45 @@
-import requests as http
-import pytest
+import pytest, requests
+
+from http_requests import get, put, delete
 
 
 def test_PersistedLmdb():
-	a = 1
 
-	assert isinstance(a, int)
+	a = get('//lmdb/test_dbi/hello')
+	assert isinstance(a, requests.models.Response)
+	assert a.status_code == 404
 
-	with pytest.raises(ZeroDivisionError):
-		b = a/0
+	d = get('//lmdb/test_dbi.new')
+	assert d.status_code == 200
 
-	assert 3*a == 3
+	n = put('//lmdb/test_dbi/twenty', 'What is 5*4?')
+	assert n.status_code == 201
+
+	n = put('//lmdb/test_dbi/five', 'What is 7 - 2?')
+	assert n.status_code == 201
+
+	a = get('//lmdb/test_dbi/twenty')
+	assert a.status_code == 200 and a.text == 'What is 5*4?'
+
+	a = get('//lmdb/test_dbi/five')
+	assert a.status_code == 200 and a.text == 'What is 7 - 2?'
+
+	d = delete('//lmdb/test_dbi/twenty')
+	assert d.status_code == 200
+
+	a = get('//lmdb/test_dbi/twenty')
+	assert a.status_code == 404
+
+	a = get('//lmdb/test_dbi/five')
+	assert a.status_code == 200 and a.text == 'What is 7 - 2?'
+
+	d = delete('//lmdb/test_dbi')
+	assert d.status_code == 200
+
+	a = get('//lmdb/test_dbi/twenty')
+	assert a.status_code == 404
+
+	a = get('//lmdb/test_dbi/five')
+	assert a.status_code == 404
+
+# test_PersistedLmdb()
