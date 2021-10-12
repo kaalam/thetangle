7c643e133cb758a7cab157d7199ed6e070818ee8
kaalam <kaalam@kaalam.ai>
2021-10-12 20:23:40 +0200

    Adds test for deque/~push

--- tests/test_api_serialization.py
+++ tests/test_api_serialization.py
@@ -46,7 +46,8 @@ def test_Serialization():
 	ret_kind = '{"palette" : BYTE[10, height], "time" : TIME[3], "author" : STRING[40], "filter" : SINGLE[height, height, 3]}'
 	ret_tupl = '("weights" : [[17, 170], [112, 54], [207, 149]], "author" : ["Billy"], "score" : [9.499999999999999556e-01])'
 	ret_tens = '[[10, 170, 99], [11, 54, 999], [12, 149, 9999], [13, 36, 9999], [14, 149, 999], [15, 44, 99]]'
-	ret_slic = ''
+	ret_sli1 = ''
+	ret_sli2 = ''
 
 	a = get('//lmdb/test_serial/kind.text')
 	assert a.status_code == 200 and a.text[:-1] == ret_kind
@@ -66,11 +67,23 @@ def test_Serialization():
 	a = get('//lmdb/test_serial/s_tens')
 	assert a.status_code == 200 and a.text == ret_tens
 
+	q = get('//deque/my_stack.new')
+	assert q.status_code == 200
+
+	a = get('//deque/m_stack/~push=//lmdb/test_serial/tens[@[3,5]]')
+	assert a.status_code == 200
+
+	a = get('//deque/m_stack/~plast.text')
+	assert a.status_code == 200 and a.text == ret_sli1
+
+	q = delete('//deque/my_stack')
+	assert q.status_code == 200
+
 	a = get('//lmdb/test_serial/slic=//lmdb/test_serial/tens[@[1,2,5]]')
 	assert a.status_code == 200
 
 	a = get('//lmdb/test_serial/slic.text')
-	assert a.status_code == 200 and a.text == ret_slic
+	assert a.status_code == 200 and a.text == ret_sli2
 
 	d = delete('//lmdb/test_serial')
 	assert d.status_code == 200
