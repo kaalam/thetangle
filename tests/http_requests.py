7997f38d50f5384a331a4773b289016c14f28e2d
kaalam <kaalam@kaalam.ai>
2021-10-20 19:02:15 +0200

    Extends to remote

--- tests/http_requests.py
+++ tests/http_requests.py
@@ -1,13 +1,23 @@
 import requests as http
 
 SERVER_URL = 'http://192.168.1.19:8899'
+REMOTE_URL = 'http://192.168.1.141:8888'
 
 
-def get(qq):
+def get(qq, remote = False):
+	if remote:
+		return http.get(REMOTE_URL + qq)
+
 	return http.get(SERVER_URL + qq)
 
-def put(qq, string):
+def put(qq, string, remote = False):
+	if remote:
+		return http.put(REMOTE_URL + qq, string)
+
 	return http.put(SERVER_URL + qq, string)
 
-def delete(qq):
+def delete(qq, remote = False):
+	if remote:
+		return http.delete(REMOTE_URL + qq)
+
 	return http.delete(SERVER_URL + qq)
