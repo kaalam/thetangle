ccc6dfa02f0cc0dd459f866bf5db35fab66b2059
kaalam <kaalam@kaalam.ai>
2021-10-20 19:04:27 +0200

    Edits serialization test

--- tests/test_api_serialization.py
+++ tests/test_api_serialization.py
@@ -46,44 +46,44 @@ def test_Serialization():
 	ret_kind = '{"palette" : BYTE[10, height], "time" : TIME[3], "author" : STRING[40], "filter" : SINGLE[height, height, 3]}'
 	ret_tupl = '("weights" : [[17, 170], [112, 54], [207, 149]], "author" : ["Billy"], "score" : [9.499999999999999556e-01])'
 	ret_tens = '[[10, 170, 99], [11, 54, 999], [12, 149, 9999], [13, 36, 9999], [14, 149, 999], [15, 44, 99]]'
-	ret_sli1 = ''
-	ret_sli2 = ''
+	ret_sli1 = '[[13, 36, 9999], [15, 44, 99]]'
+	ret_sli2 = '[[11, 54, 999], [12, 149, 9999], [15, 44, 99]]'
 
 	a = get('//lmdb/test_serial/kind.text')
 	assert a.status_code == 200 and a.text[:-1] == ret_kind
 
 	a = get('//lmdb/test_serial/s_kind')
-	assert a.status_code == 200 and a.text == ret_kind
+	assert a.status_code == 200 and a.text[:-1] == ret_kind
 
 	a = get('//lmdb/test_serial/tupl.text')
 	assert a.status_code == 200 and a.text[:-1] == ret_tupl
 
 	a = get('//lmdb/test_serial/s_tupl')
-	assert a.status_code == 200 and a.text == ret_tupl
+	assert a.status_code == 200 and a.text[:-1] == ret_tupl
 
 	a = get('//lmdb/test_serial/tens.text')
 	assert a.status_code == 200 and a.text[:-1] == ret_tens
 
 	a = get('//lmdb/test_serial/s_tens')
-	assert a.status_code == 200 and a.text == ret_tens
+	assert a.status_code == 200 and a.text[:-1] == ret_tens
 
 	q = get('//deque/my_stack.new')
 	assert q.status_code == 200
 
-	a = get('//deque/m_stack/~last=//lmdb/test_serial/tens[@[3,5]]')
+	a = get('//deque/my_stack/~last=//lmdb/test_serial/tens[&[3,5]]')
 	assert a.status_code == 200
 
-	a = get('//deque/m_stack/~plast.text')
-	assert a.status_code == 200 and a.text == ret_sli1
+	a = get('//deque/my_stack/~plast.text')
+	assert a.status_code == 200 and a.text[:-1] == ret_sli1
 
 	q = delete('//deque/my_stack')
 	assert q.status_code == 200
 
-	a = get('//lmdb/test_serial/slic=//lmdb/test_serial/tens[@[1,2,5]]')
+	a = get('//lmdb/test_serial/slic=//lmdb/test_serial/tens[&[1,2,5]]')
 	assert a.status_code == 200
 
 	a = get('//lmdb/test_serial/slic.text')
-	assert a.status_code == 200 and a.text == ret_sli2
+	assert a.status_code == 200 and a.text[:-1] == ret_sli2
 
 	d = delete('//lmdb/test_serial')
 	assert d.status_code == 200
@@ -92,4 +92,7 @@ def test_Serialization():
 	assert a.status_code == 404
 
 
-# test_Serialization()
+if __name__ == '__main__':
+	test_Serialization()
+
+	print('\n\nDone.')
