2e7acab2de91378931e3d23e4ed90128b3257227
kaalam <kaalam@kaalam.ai>
2021-10-20 19:02:49 +0200

    Adds a very early stage test

--- tests/test_api_across_nodes.py
+++ tests/test_api_across_nodes.py
@@ -4,11 +4,53 @@ from http_requests import get, put, delete
 
 
 def test_AcrossNodes():
-	a = 1
+	d = get('///424x4//lmdb/test_dbi.new')
+	assert d.status_code == 200
 
-	assert isinstance(a, int)
+	n = put('///424x4//lmdb/test_dbi/twenty', 'What is 5*4?')
+	assert n.status_code == 201
 
-	with pytest.raises(ZeroDivisionError):
-		b = a/0
+	n = put('///424x4//lmdb/test_dbi/five', 'What is 7 - 2?')
+	assert n.status_code == 201
 
-	assert 3*a == 3
+	a = get('///424x4//lmdb/test_dbi/twenty')
+	assert a.status_code == 200 and a.text == 'What is 5*4?'
+
+	a = get('//lmdb/test_dbi/twenty', remote = True)
+	assert a.status_code == 200
+
+	a = get('//lmdb/test_dbi/five', remote = True)
+	assert a.status_code == 200
+
+	a = get('///424x4//lmdb/test_dbi/five')
+	assert a.status_code == 200 and a.text == 'What is 7 - 2?'
+
+	d = delete('///424x4//lmdb/test_dbi/twenty')
+	assert d.status_code == 200
+
+	a = get('//lmdb/test_dbi/twenty')
+	assert a.status_code == 404
+
+	a = get('//lmdb/test_dbi/twenty', remote = True)
+	assert a.status_code == 404
+
+	a = get('///424x4//lmdb/test_dbi/twenty')
+	assert a.status_code == 404
+
+	a = get('///424x4//lmdb/test_dbi/five')
+	assert a.status_code == 200 and a.text == 'What is 7 - 2?'
+
+	d = delete('///424x4//lmdb/test_dbi')
+	assert d.status_code == 200
+
+	a = get('///424x4//lmdb/test_dbi/twenty')
+	assert a.status_code == 404
+
+	a = get('///424x4//lmdb/test_dbi/five')
+	assert a.status_code == 404
+
+
+if __name__ == '__main__':
+	test_AcrossNodes()
+
+	print('\n\nDone.')
