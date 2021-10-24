3b8410a81c6f0dc291b2ff437e0c6bcfebe31eca
kaalam <kaalam@kaalam.ai>
2021-10-24 21:08:55 +0200

    Implements test_ChannelZmq

--- tests/test_api_channel.py
+++ tests/test_api_channel.py
@@ -4,14 +4,29 @@ from http_requests import get, put, delete
 
 
 def test_ChannelZmq():
-	a = 1
+	q = get('//deque/my_stack.new')
+	assert q.status_code == 200
 
-	assert isinstance(a, int)
+	a = put('//0-mq&pipeline/alpha;', 'tcp://localhost:5555')
+	assert a.status_code == 201
 
-	with pytest.raises(ZeroDivisionError):
-		b = a/0
+	a = get('//0-mq&pipeline/alpha;')
+	assert a.status_code == 200 and a.text == 'tcp://localhost:5555'
 
-	assert 3*a == 3
+	a = get('//deque/my_stack/~last=//0-mq/alpha/(&a b c & Z P T - 456 = .)')
+	assert a.status_code == 200
+
+	a = get('//deque/my_stack/~plast.text')
+	assert a.status_code == 200
+	ii = eval(a.text)
+	s = ''.join([chr(i) for i in ii])
+	assert s[0:9] == 'abcZPT456'
+
+	a = delete('//0-mq&pipeline/alpha;')
+	assert a.status_code == 200
+
+	q = delete('//deque/my_stack')
+	assert q.status_code == 200
 
 
 def test_ChannelBash():
