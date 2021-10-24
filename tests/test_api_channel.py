6a73f1a8a886292cb5d45f9d083473502a41c69d
kaalam <kaalam@kaalam.ai>
2021-10-24 21:09:24 +0200

    Adds more support for test_ChannelBash

--- tests/test_api_channel.py
+++ tests/test_api_channel.py
@@ -30,9 +30,24 @@ def test_ChannelZmq():
 
 
 def test_ChannelBash():
-	a = get('//bash/exec/(&ls -la)')
+	a = get('//bash/exec/(&echo "2 + 2 = $(expr 2 + 2)")')
 	assert isinstance(a, requests.models.Response)
 	assert a.status_code == 200
+
+	q = get('//deque/my_stack.new')
+	assert q.status_code == 200
+
+	a = get('//deque/my_stack/~last=//bash/exec/(&echo "2 + 2 = $(expr 2 + 2)")')
+	assert a.status_code == 200
+
+	a = get('//deque/my_stack/~plast.text')
+	assert a.status_code == 200
+	ii = eval(a.text)
+	s = ''.join([chr(i) for i in ii])
+	assert s[0:9] == '2 + 2 = 4'
+
+	q = delete('//deque/my_stack')
+	assert q.status_code == 200
 
 
 def test_ChannelFile():
