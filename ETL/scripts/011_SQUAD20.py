2ff2b1aada0e681f9fe10bbeff9d02ce8aba9555
Jacques <jacques.basaldua@gmail.com>
2021-11-23 16:24:08 +0000

    Refactors SQUAD20

--- ETL/scripts/011_SQUAD20.py
+++ ETL/scripts/011_SQUAD20.py
@@ -1,23 +1,17 @@
-import json
-from os import replace
-import re
+import json, re
 
-
-def clean(s):
-	while s.find('  ') >=0: s = s.replace('  ', ' ')
-	while s.find(' ,') >=0: s = s.replace(' ,', ',')
-	while s.find(' .') >=0: s = s.replace(' .', '.')
-	return s.strip()
+from file_paths import etl_source, etl_dest
+from Section import Section
 
 
 class SQUAD20:
 
-	def __init__(self, source_fn = '/home/jadmin/kaalam.etc/nlp/corpora/SQuAD/train-v2.0.json', out_path  = './'):
+	def __init__(self, source_fn = etl_source + '/SQuAD/train-v2.0.json', out_path = etl_dest):
 		self.source_fn = source_fn
-		self.out_contexts  = out_path + 'squad20/context.txt'
-		self.out_qa_index  = out_path + 'squad20/qa_index.txt'
-		self.out_questions = out_path + 'squad20/question.txt'
-		self.out_answers   = out_path + 'squad20/answer.txt'
+		self.context   = Section('squad20', 'context', out_path + '/squad20')
+		self.qa_index  = Section('squad20', 'qa-index', out_path + '/squad20')
+		self.question  = Section('squad20', 'question', out_path + '/squad20')
+		self.answer	   = Section('squad20', 'answer', out_path + '/squad20')
 
 
 	def inputs(self):
@@ -30,38 +24,30 @@ class SQUAD20:
 
 	def build(self):
 		f_in = open(self.source_fn, 'r')
-		f_ct = open(self.out_contexts, 'w')
-		f_ii = open(self.out_qa_index, 'w')
-		f_qq = open(self.out_questions, 'w')
-		f_aa = open(self.out_answers, 'w')
-
-		rex_cl = re.compile('[^a-z0-9\'\\-,\\.\\? ]')
 
 		obj = json.load(f_in)
 
 		ctx_row = 1
 		for item in obj['data']:
 			for row in item['paragraphs']:
-				ctx = clean(rex_cl.sub(' ', row['context'].lower()))
-				f_ct.write(ctx + '\n')
+				self.context.write_line(row['context'])
 
 				for qa in row['qas']:
-					qq = clean(rex_cl.sub(' ', qa['question'].lower()))
-					f_qq.write(qq + '\n')
-					f_ii.write(str(ctx_row) + '\n')
+					self.question.write_line(qa['question'])
+					self.qa_index.write_line(str(ctx_row))
+
 					if qa['is_impossible']:
-						f_aa.write('\n')
+						self.answer.write_line('(IMPOSSIBLE)')
 					else:
-						aa = clean(rex_cl.sub(' ', qa['answers'][0]['text'].lower()))
-						f_aa.write(aa + '\n')
+						self.answer.write_line(qa['answers'][0]['text'])
 
 				ctx_row += 1
 
 		f_in.close()
-		f_ct.close()
-		f_ii.close()
-		f_qq.close()
-		f_aa.close()
+		self.context.close()
+		self.qa_index.close()
+		self.question.close()
+		self.answer.close()
 
 
 c = SQUAD20()
