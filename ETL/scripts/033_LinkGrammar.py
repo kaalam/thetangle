fafd844013bd364470c376ed0e57d92299e7f5ac
Jacques <jacques.basaldua@gmail.com>
2021-11-24 15:21:55 +0000

    Refactors LinkGrammar

--- ETL/scripts/033_LinkGrammar.py
+++ ETL/scripts/033_LinkGrammar.py
@@ -1,26 +1,51 @@
-import re
+import os, re
+
+
+from file_paths import etl_source, etl_dest
+from Section import Section
 
 
 class LinkGrammar:
 
-	def __init__(self, in_path = '/home/jadmin/kaalam.github/neat-lg/neatlg/data/', out_path  = './'):
+	def __init__(self, in_path = etl_source + '/LinkGrammar/words', out_path = etl_dest):
 		self.in_path = in_path
-		self.out_word_lnk = out_path + 'linkgrammar/links.txt'
-		self.out_word_pos = out_path + 'linkgrammar/pos.txt'
-		self.out_words = out_path + 'indices/words.linkgram'
-		self.out_links = out_path + 'indices/links.linkgram'
-
-
-	def inputs(self):
-		return [self.in_path + '*']
-
-
-	def outputs(self):
-		return [self.out_word_lnk, self.out_word_pos, self.out_words, self.out_links]
+		self.words	 = Section('LinkGrammar', 'word', out_path + '/LinkGrammar', num_rows = 10000)
 
 
 	def build(self):
-		pass
+		rex_fn	  = re.compile('^words.*$')
+		rex_split = re.compile('^([A-Za-z]+)\\.[A-Za-z]+$')
+		rex_word  = re.compile('^[A-Za-z]+$')
+		words	  = set()
+		names	  = os.listdir(self.in_path)
+		names.sort()
+
+		for fn in names:
+			if rex_fn.match(fn):
+				print(fn, ',', end = ' ', flush = True)
+				f_in = open('%s/%s' % (self.in_path, fn), 'r', encoding = 'ISO-8859-1')
+				line = f_in.readline()
+
+				while line != '':
+					line = line.strip()
+					if line != '':
+						ww = line.split(' ')
+						for w in ww:
+							if rex_split.match(w):
+								w = rex_split.sub('\\1', w)
+							if rex_word.match(w):
+								words.add(w)
+
+					line = f_in.readline()
+
+				f_in.close()
+
+		wl = list(words)
+		wl.sort()
+		for w in wl:
+			self.words.write_line(w)
+
+		self.words.close()
 
 
 c = LinkGrammar()
