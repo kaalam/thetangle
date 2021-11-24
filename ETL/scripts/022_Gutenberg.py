7a4e2f649eb604444ede5f12c41465ccb8a196cb
Jacques <jacques.basaldua@gmail.com>
2021-11-24 10:37:00 +0000

    Refactors Gutenberg

--- ETL/scripts/022_Gutenberg.py
+++ ETL/scripts/022_Gutenberg.py
@@ -1,40 +1,33 @@
 import os
 
 
+from file_paths import etl_source, etl_dest
+from Section import Section
+
+
 class Gutenberg:
 
 	def __init__(self,
-				 in_tit = '/home/jadmin/kaalam.etc/nlp/corpora/gutenberg/index-titles.txt',
-				 in_idx = '/home/jadmin/kaalam.etc/nlp/corpora/gutenberg/index-blocks.txt',
-				 in_pat = '/home/jadmin/kaalam.etc/nlp/corpora/gutenberg/books/',
-				 oo_pat = 'gutenberg/'):
+				 in_tit	  = etl_source + '/gutenberg/index-titles.txt',
+				 in_idx	  = etl_source + '/gutenberg/index-blocks.txt',
+				 in_pat	  = etl_source + '/gutenberg/books/',
+				 out_path = etl_dest):
 
 		self.in_tit = in_tit
 		self.in_idx = in_idx
 		self.in_pat = in_pat
-		self.oo_pat = oo_pat
-
-
-	def inputs(self):
-		return [self.in_tit, self.in_idx, self.in_pat + '*']
-
-
-	def outputs(self):
-		return [self.oo_pat + '*']
+		self.title	= Section('gutenberg', 'title', out_path + '/gutenberg')
+		self.book	= Section('gutenberg', 'book', out_path + '/gutenberg', num_rows = 10)
 
 
 	def build(self):
 		f_tit = open(self.in_tit, 'r')
 		f_idx = open(self.in_idx, 'r')
-		fo_tt = open(self.oo_pat + 'index-titles.txt', 'w')
-		fo_ix = open(self.oo_pat + 'index-blocks.txt', 'w')
 
 		tt = f_tit.readline()
 		xx = f_idx.readline()
 
 		assert ((tt == '') == (xx == ''))
-
-		blocks = set()
 
 		t_ll = 0
 
@@ -46,29 +39,20 @@ class Gutenberg:
 			tt = tt.strip()
 			xx = xx.strip()
 
-			fn_in = "%s%s.RData" % (self.in_pat, xx)
+			self.title.write_line(tt)
 
-			if len(xx) < 30:
-				blocks.add(xx)
-			else:
-				xx = xx[0:28]
-				for i in range(101):
-					if i == 100:
-						raise AssertionError
+			fn_in  = "%s%s.RData" % (self.in_pat, xx)
+			fn_out = "./book_text.txt"
 
-					if xx + str(i) not in blocks:
-						xx = xx + str(i)
-						blocks.add(xx)
-						break
-
-			fn_out = "%s%s.txt" % (self.oo_pat, xx)
-
-			cmd = "/home/jadmin/kaalam.etc/nlp/corpora/gutenberg/cat.R %s > %s" % (fn_in, fn_out)
+			cmd = "%s/gutenberg/cat.R %s > %s" % (etl_source, fn_in, fn_out)
 
 			os.system(cmd)
 
-			fo_tt.write(tt + '\n')
-			fo_ix.write(xx + '\n')
+			with open(fn_out, 'r', encoding = 'ISO-8859-1') as f:
+				book = f.read()
+				self.book.write_line(book)
+
+			os.remove(fn_out)
 
 			tt = f_tit.readline()
 			xx = f_idx.readline()
@@ -77,8 +61,8 @@ class Gutenberg:
 
 		f_tit.close()
 		f_idx.close()
-		fo_tt.close()
-		fo_ix.close()
+		self.title.close()
+		self.book.close()
 
 
 c = Gutenberg()
