a3c3a2523354594841dcc505608c6ae94b7069a7
Jacques <jacques.basaldua@gmail.com>
2021-12-24 18:51:34 +0000

    Sets for debugging the grammar parts

--- ETL/scripts/file_paths.py
+++ ETL/scripts/file_paths.py
@@ -2,7 +2,7 @@ import os
 
 
 # os.environ['TANGLE_ETL_SOURCE'] = '/home/jadmin/kaalam.etc/nlp/corpora'
-# os.environ['TANGLE_ETL_DEST']   = '/home/jadmin/kaalam.github/tng-data-qa/data'
+# os.environ['TANGLE_ETL_DEST']   = '/home/jadmin/kaalam.github/tng-data-grammar/data'
 
 
 etl_source = os.environ['TANGLE_ETL_SOURCE']
