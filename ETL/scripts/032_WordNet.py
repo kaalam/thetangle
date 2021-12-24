ad1e0b4791a9c1471fb547808eaa5cb89e72d678
Jacques <jacques.basaldua@gmail.com>
2021-12-24 18:50:33 +0000

    Refactors to make it a graph with three sections

--- ETL/scripts/032_WordNet.py
+++ ETL/scripts/032_WordNet.py
@@ -13,9 +13,14 @@ class EchoTarget:
 	def __init__(self, out_path = etl_dest):
 		self.level	= 0
 		self.ent_id	= None
+		self.form	= None
+		self.pos	= None
+		self.ss_id	= None
+		self.synset	= None
+		self.tx_pos	= Section('WordNet', 'tx-pos', out_path + '/WordNet', num_rows = 10000)
+		self.tx_syn	= Section('WordNet', 'tx-syn', out_path + '/WordNet', num_rows = 10000)
+		self.tx_rel	= Section('WordNet', 'tx-rel', out_path + '/WordNet', num_rows = 10000)
 		self.t_ll	= 0
-		self.form	= Section('WordNet', 'write-as', out_path + '/WordNet', num_rows = 10000)
-		self.pos	= Section('WordNet', 'pos', out_path + '/WordNet', num_rows = 10000)
 
 
 	def start(self, tag, attrib):
@@ -23,48 +28,62 @@ class EchoTarget:
 		self.level += 1
 
 		if tag == 'LexicalEntry':
-			assert self.level == 3
+			assert self.level == 3 and self.ent_id is None
 			self.ent_id = attrib['id']
 
 			return
 
 		if tag == 'Lemma':
-			assert self.level == 4
-			if 'writtenForm' in attrib and 'partOfSpeech' in attrib:
-				self.t_ll += 1
-				if self.t_ll % 10000 == 0:
-					print('%0.2fM,' % (self.t_ll/1000000), end = ' ', flush = True)
+			assert self.level == 4 and self.ent_id is not None and self.form is None and self.pos is None
 
-				self.form.write_line(attrib['writtenForm'])
-				self.pos.write_line(attrib['partOfSpeech'])
-			else:
-				assert False
+			self.form = attrib['writtenForm']
+			self.pos  = attrib['partOfSpeech']
+
+			self.t_ll += 1
+			if self.t_ll % 10000 == 0:
+				print('%0.2fM,' % (self.t_ll/1000000), end = ' ', flush = True)
+
+			self.tx_pos.write_line('%s, %s, %s' % (self.form, self.pos, self.ent_id))
 
 			return
 
 		if tag == 'Sense':
-			assert self.level == 4
-			if 'id' in attrib and 'synset' in attrib:
-				""" This is a previous clarification that applies to the following edge. """
-			else:
-				assert False
+			assert self.level == 4 and self.ent_id is not None and self.form is None and self.pos is None
+
+			self.ss_id	= attrib['id']
+			self.synset	= attrib['synset']
+
+			self.tx_syn.write_line('%s, %s, %s' % (self.ent_id, self.ss_id, self.synset))
 
 			return
 
 		if tag == 'SenseRelation':
-			assert self.level == 5
-			if 'relType' in attrib and 'target' in attrib:
-				""" This is an edge in the graph!!! """
-			else:
-				assert False
+			assert (self.level == 5 and self.ent_id is not None and self.form is None and self.pos is None
+					and self.ss_id is not None and self.synset is not None)
+
+			self.relation = attrib['relType']
+			self.target	  = attrib['target']
+
+			self.tx_rel.write_line('%s, %s, %s, %s' % (self.ent_id, self.ss_id, self.relation, self.target))
 
 			return
 
 
 	def end(self, tag):
 		self.level -= 1
-		if self.level < 3:
+
+		if tag == 'LexicalEntry':
 			self.ent_id = None
+			return
+
+		if tag == 'Lemma':
+			self.form = None
+			self.pos  = None
+			return
+
+		if tag == 'Sense':
+			self.ss_id	= None
+			self.synset	= None
 
 
 	def data(self, data):
@@ -72,8 +91,9 @@ class EchoTarget:
 
 
 	def close(self):
-		self.form.close()
-		self.pos.close()
+		self.tx_pos.close()
+		self.tx_syn.close()
+		self.tx_rel.close()
 
 		return "closed!"
 
