00ba768174c827a24d59fe66487801d09188058a
Jacques <jacques.basaldua@gmail.com>
2021-12-26 14:42:29 +0000

    Sets raiders repo by default

--- ETL/scripts/file_paths.py
+++ ETL/scripts/file_paths.py
@@ -2,7 +2,7 @@ import os
 
 
 # os.environ['TANGLE_ETL_SOURCE'] = '/home/jadmin/kaalam.etc/nlp/corpora'
-# os.environ['TANGLE_ETL_DEST']   = '/home/jadmin/kaalam.github/tng-data-grammar/data'
+# os.environ['TANGLE_ETL_DEST']   = '/home/jadmin/kaalam.github/tng-data-raiders/data'
 
 
 etl_source = os.environ['TANGLE_ETL_SOURCE']
